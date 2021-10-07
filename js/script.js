08b9c08f93853f157fb84609e28acaef083ce6f8
typos-r-us <kennethmwenda@gmail.com>
2021-10-08 02:00:58 +0300

    Added footer section, scroll-to-top, typing animation. Fixed fullscreen menu display issue.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,36 @@ $(document).ready(function (){
     }else {
       $('.navbar').removeClass("sticky");
     }
+    if (this.scroll > 500){
+      $('.scroll-top-btn').addClass("show");
+    } else {
+      $('.scroll-top-btn').removeClass("show");
+    }
+  });
+  // Slide up script
+  $('.scroll-top-btn').click(function (){
+    $('html').animate({
+      scrollTop:0,
+    });
   });
   // toggle menu button/navbar
   $('.menu-btn').click(function (){
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
     console.log("clicked.");
+  });
+  // typing animation script
+  let typed = new Typed(".typing",{
+    strings: ["Network Engineer.","Android Developer.","Web Designer.","Technical Supporter."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+  let typed2 = new Typed(".typing-2",{
+    strings: ["Network Engineer.","Android Developer.","Web Designer.","Technical Supporter."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
   });
   // owl carousel script
   $('.carousel').owlCarousel({
